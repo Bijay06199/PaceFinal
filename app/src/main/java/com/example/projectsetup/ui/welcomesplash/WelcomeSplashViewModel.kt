b8e41000d132e317655b2a48c831dcc13b54e868
Bijay Bastakoti <bijaybastakoti111@gmail.com>
2020-03-18 11:31:25 +0545

    FirstCommit

---
+++ app/src/main/java/com/example/projectsetup/ui/welcomesplash/WelcomeSplashViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.projectsetup.ui.welcomesplash
+
+import com.example.projectsetup.base.BaseViewModel
+
+class WelcomeSplashViewModel:BaseViewModel() {
+
+    val next:String=""
+    val skip:String=""
+}
